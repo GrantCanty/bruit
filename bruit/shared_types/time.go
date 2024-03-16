19ff2231ea1146242c765125d2502406e8b9ba71
GrantCanty <gc933815@ohio.edu>
2024-03-16 00:53:17 -0600

    changed UnixTime unmarshaller to accept both string and float types. needs redone eventually

--- bruit/shared_types/time.go
+++ bruit/shared_types/time.go
@@ -3,6 +3,7 @@ package shared_types
 import (
 	"encoding/json"
 	"math"
+	"strconv"
 	"time"
 )
 
@@ -10,9 +11,47 @@ type UnixTime struct {
 	time.Time
 }
 
+//starts by trying to unmarsal using timestamp as a string. if it fails, trying to unmarshal using ts as float
 func (u *UnixTime) UnmarshalJSON(d []byte) error {
-	var ts float64
+	var stringTS string
+	err := json.Unmarshal(d, &stringTS)
+	if err != nil {
+		var floatTS float64
+		err = json.Unmarshal(d, &floatTS)
+		if err != nil {
+			return err
+		}
+		sec, min := math.Modf(floatTS)
+		u.Time = time.Unix(int64(sec), int64(min)).UTC()
+		u.Time.Unix()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	floatTime, err := strconv.ParseFloat(stringTS, 64)
+	if err != nil {
+		return err
+	}
+	sec, min := math.Modf(floatTime)
+	u.Time = time.Unix(int64(sec), int64(min)).UTC()
+	u.Time.Unix()
+	if err != nil {
+		return err
+	}
+	return nil
+
+	// below is old func
+
+	//var ts string
+	/*var ts float64
 	err := json.Unmarshal(d, &ts)
+	if err != nil {
+		log.Println("error hereeee")
+		return err
+	}
+	floatTime, err := strconv.ParseFloat(ts, 64)
 	if err != nil {
 		return err
 	}
@@ -22,5 +61,5 @@ func (u *UnixTime) UnmarshalJSON(d []byte) error {
 	if err != nil {
 		return err
 	}
-	return nil
+	return nil*/
 }
