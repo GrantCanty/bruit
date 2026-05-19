4a4356695714ce23dbd8b36969bf22801d756364
Grant Canty <gc933815@ohio.edu>
2026-05-20 00:32:27 +0200

    edited state_manager file to implement safer locking flows

--- bruit/clients/kraken/state/state_manager.go
+++ bruit/clients/kraken/state/state_manager.go
@@ -34,55 +34,31 @@ func (sm *StateManager) OnOHLCResponse(data types.OHLCResponse, ohlcMap *shared_
 }
 
 func (sm *StateManager) handleOHLCResponse(resp types.OHLCResponse, ohlcMap *shared_types.OHLCVals) {
-	ohlcMap.RLock()
+	ohlcMap.Lock()
+	defer ohlcMap.Unlock()
+
 	if list, found := ohlcMap.GetData()[resp.GetMetaData()]; found { // if channelID already exists in the map, then...
-
 		var tmpStartTime time.Time = resp.OHLCArray.EndTime.Add(-time.Minute * time.Duration(sm.GetInterval(resp.GetMetaData())))
-		ohlcMap.RUnlock()
-
-		ohlcMap.Lock()
 		resp.OHLCArray.SetStartTime(tmpStartTime)
-		ohlcMap.Unlock()
 
-		ohlcMap.RLock()
 		if list.GetList().IsEmpty() { // if no responses
 			node := shared_types.Node{Data: &resp.OHLCArray, Next: nil}
-			ohlcMap.RUnlock()
-
-			ohlcMap.Lock()
 			list.GetList().AddToEnd(&node)
-			ohlcMap.Unlock()
 		} else if list.GetList().GetLast().Data.GetEndTime().Time.Equal(resp.OHLCArray.EndTime.Time) { // if updating last candle
 			tmp := resp.OHLCArray
-			ohlcMap.RUnlock()
-
-			ohlcMap.Lock()
 			list.GetList().EditCandle(list.GetList().GetLast().Data, &tmp)
-			ohlcMap.Unlock()
 		} else if list.GetList().GetLast().Data.GetEndTime().Time.Equal(resp.OHLCArray.StartTime.Time) { // adding candle to next index in array
 			node := shared_types.Node{Data: &resp.OHLCArray, Next: nil}
-			ohlcMap.RUnlock()
-
-			ohlcMap.Lock()
 			list.GetList().AddToEnd(&node)
-			ohlcMap.Unlock()
 		} else if list.GetList().GetLast().Data.GetEndTime().Time.Before(resp.OHLCArray.StartTime.Time) { // if adding multiple candles
 			tmp := resp.OHLCArray
-			ohlcMap.RUnlock()
-
-			ohlcMap.Lock()
 			list.GetList().AddCandle(&tmp, &types.WSCandles{}, sm.GetInterval(resp.GetMetaData()))
-			ohlcMap.Unlock()
 		}
 	} else { // if the channel id cannot be found in the map
 		interval, _ := strconv.ParseInt(resp.ChannelName[len(resp.ChannelName)-1:], 10, 64)
 		resp.OHLCArray.StartTime.Time = resp.OHLCArray.EndTime.Add(-time.Minute * time.Duration(interval))
 		node := shared_types.Node{Data: &resp.OHLCArray, Next: nil}
 		tmp := &shared_types.List{Head: &node, Last: &node, Length: 1}
-		ohlcMap.RUnlock()
-
-		ohlcMap.Lock()
 		ohlcMap.Set(resp.GetMetaData(), tmp)
-		ohlcMap.Unlock()
 	}
 }
